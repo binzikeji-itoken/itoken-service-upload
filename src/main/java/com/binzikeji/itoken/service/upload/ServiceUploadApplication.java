8539f9bbcb6c61033166e2ae0f5b29233c41d4ea
ShuaiBin <3166688833@qq.com>
2019-06-02 13:11:17 +0800

    init

---
+++ src/main/java/com/binzikeji/itoken/service/upload/ServiceUploadApplication.java
@@ -0,0 +1,17 @@
+package com.binzikeji.itoken.service.upload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description
+ * @Author Bin
+ * @Date 2019/5/30 16:24
+ **/
+@SpringBootApplication
+public class ServiceUploadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceUploadApplication.class, args);
+    }
+}
